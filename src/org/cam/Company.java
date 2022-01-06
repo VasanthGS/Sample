37ba4622cb2c9b685d60d3e2baccfa1a5db2f8e7
Vasanth <gsvasanth06@gmail.com>
2022-01-06 18:56:52 +0530

    done my project

---
+++ src/org/cam/Company.java
@@ -0,0 +1,16 @@
+package org.cam;
+
+public class Company {
+	private void companyName() {
+		System.out.println(" Collabera");
+	}
+	private void companyAddress() {
+		System.out.println("chennai");
+	}
+	public static void main(String[] args) {
+		Company c = new Company();
+		c.companyName();
+		c.companyAddress();
+	}
+
+}
